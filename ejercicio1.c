f5c5ccdd18a3bdb88bd19168ef643b32f5d8e4d4
gemece <30463151+gemece@users.noreply.github.com>
2017-11-09 16:27:26 +0100

    Update ejercicio1.c

--- ejercicio1.c
+++ ejercicio1.c
@@ -2,6 +2,26 @@
 int main(void){
 
 char a, b, c;
-
+input("Introduce una palabra de tres letras: ");
+a=getchar();
+b=getchar();
+c=getchar();
+getchar();
+ ////aqui para pedir la palabra
+input("Introduce una laetra: \n");
+char letra;
+letra=getchar();
+if(letra==a){
+  input("letra %c correcta\n",a);
+}
+if(letra==b){
+input("letra %c correcta\n",b);
+}
+if(letra==c){
+input("letra %c correcta\n",c);
+}
+else{
+  input("nignua letra correcta\n");
+}
 return 0;
 }
