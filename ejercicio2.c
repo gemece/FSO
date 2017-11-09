538b1b3e4a59721a5ffaf4ad5d28c070c59cd381
gemece <30463151+gemece@users.noreply.github.com>
2017-11-09 17:37:24 +0100

    Create ejercicio2.c

---
+++ ejercicio2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void){
+float a,b;
+printf("Introduce los dos numeros: ");
+scanf("%f %f", &a &b);
+int cont=0;
+do{
+if(a>b){
+   a=a/10;
+   if(a<b){
+   a=a*10
+   cont=1;
+   }
+   if(1>a>0){
+   a=a*10;
+   cont=1;
+   }
+  }while(cont=0);
+  return 0;
+  }
