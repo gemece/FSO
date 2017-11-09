663f7d207f8eacce150e36cc1083a15b1fddd8f7
gemece <30463151+gemece@users.noreply.github.com>
2017-11-09 16:30:49 +0100

    Update ejercicio1.c

--- ejercicio1.c
+++ ejercicio1.c
@@ -8,20 +8,20 @@ b=getchar();
 c=getchar();
 getchar();
  ////aqui para pedir la palabra
-input("Introduce una laetra: \n");
+printf("Introduce una laetra: \n");
 char letra;
 letra=getchar();
 if(letra==a){
-  input("letra %c correcta\n",a);
+  printf("letra %c correcta\n",a);
 }
 if(letra==b){
-input("letra %c correcta\n",b);
+printf("letra %c correcta\n",b);
 }
 if(letra==c){
-input("letra %c correcta\n",c);
+printf("letra %c correcta\n",c);
 }
 else{
-  input("nignua letra correcta\n");
+printf("nignua letra correcta\n");
 }
 return 0;
 }
